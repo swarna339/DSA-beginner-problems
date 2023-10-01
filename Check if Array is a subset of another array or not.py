02efd515a18dccc83610bc893fd9226f67320f8c
SANGANA SWARNA <ucs20339@rmd.ac.in>
2023-10-01 20:40:38 +0530

    Create Check if Array is a subset of another array or not.py

---
+++ Check if Array is a subset of another array or not.py
@@ -0,0 +1,12 @@
+# Input data
+arr1 = [1,3,6,7,10]
+arr2 = [1,2,3,4,5,6,7,8]
+set1=set(arr1)
+set2=set(arr2)
+
+if set1.issubset(set2):
+    print("IT IS A SUBSET")
+else:
+    print("IT IS NOT A SUBSET")
+
+OUTPUT:IT IS NOT A SUBSET
